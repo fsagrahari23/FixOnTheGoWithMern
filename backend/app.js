54ce60e2bd94a909a9a0f09a45f0bf7bd9fc838b
fsagrahari23 <monu.a23@iiits.in>
2026-04-18 20:32:14 +0530

    fixed cors

--- backend/app.js
+++ backend/app.js
@@ -144,12 +144,10 @@ if (process.env.NODE_ENV === 'production') {
 
 
 
-app.use(cors(
-  {
-    origin: true,
-    credentials: true
-  }
-))
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  credentials: true
+}));
 
 // Passport middleware
 app.use(passport.initialize());
